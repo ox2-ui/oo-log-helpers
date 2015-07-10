71415caa40775ea244f4269cdbfc02a50900c89f
Lauricio Su <me@lauricio.com>
2015-07-10 13:16:33 +0800

    updated summary

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:log-helpers',
-  summary: 'Helper functions for pretty print output',
+  summary: 'Helper functions for pretty console.log output',
   version: '1.0.0',
   git: ' /* Fill me in! */ '
 });
